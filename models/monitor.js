9fbeca1f00b4d883ed83636b0c05b368fb41d9ed
park-david <davidpark1094@gmail.com>
2023-07-04 23:27:24 -0700

    fixed home, added missing seed value

--- models/monitor.js
+++ models/monitor.js
@@ -7,7 +7,7 @@ const monitorSchema = new mongoose.Schema ({
     description: { type: String, required: true },
     screenSize: { type: Number, enum: [24, 27, 32, 34, 40], required: true },
     resolution: { type: String, enum: ['1920x1080', '2560x1440', '3440x1440', '3840x2160', '5120x2160'], required: true },
-    refreshRate: { type: Number, enum: [60, 72, 75, 144, 165, 180, 240], required: true },
+    refreshRate: { type: Number, enum: [60, 72, 75, 144, 165, 175, 180, 240], required: true },
     responseTime: { type: String, enum: ['0.03ms' ,'0.3ms', '0.5ms', '1ms', '4ms', '5ms'], required: true },
     panelType: { type: String, enum: ['IPS', 'VA', 'TN', 'OLED', 'QD-OLED'], required: true },
     curved: { type: Boolean, default: false, required: true },
